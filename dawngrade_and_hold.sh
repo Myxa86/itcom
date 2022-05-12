e7711de0d506acad68bc3524bfd9ce1d3d6e243e
Roman <kosux.roman@gmail.com>
2022-05-12 20:23:23 +0300

    Update dawngrade_and_hold.sh

--- dawngrade_and_hold.sh
+++ dawngrade_and_hold.sh
@@ -2,5 +2,5 @@ sudo apt-mark hold linux-headers*
 sudo apt-mark hold linux-hwe*
 sudo apt-mark hold linux-image*
 sudo apt-mark hold linux-modules*
-#sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT="1>2"/g' /etc/default/grub
+sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT="1>2"/g' /etc/default/grub
 sudo update-grub
