4901571fb501bce1418309fa38fb09a5e82bb444
Roman <kosux.roman@gmail.com>
2022-07-20 19:17:19 +0300

    Add files via upload

--- dawngrade_and_hold.sh
+++ dawngrade_and_hold.sh
@@ -1,6 +1,6 @@
-sudo apt-mark hold linux-headers*
-sudo apt-mark hold linux-hwe*
-sudo apt-mark hold linux-image*
-sudo apt-mark hold linux-modules*
+sudo apt-mark hold linux-headers* \
+    linux-hwe* \
+    linux-image* \
+    linux-modules*
 sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT="1>2"/g' /etc/default/grub
 sudo update-grub
