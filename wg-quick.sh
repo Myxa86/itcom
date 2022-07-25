5dac2885dd86969b9b87418071ef7f2276f73a1c
Roman <kosux.roman@gmail.com>
2022-07-25 15:44:31 +0300

    Update wg-quick.sh

--- wg-quick.sh
+++ wg-quick.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo apt update && sudo apt install wireguard resolvconf -y
 sudo cp ~/Downloads/*conf /etc/wireguard/wg0.conf
 sudo systemctl enable --now wg-quick@wg0.service
